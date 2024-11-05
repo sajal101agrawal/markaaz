c42849100295e6a34092b14700a85bb12b50e254
Faizal Shaikh <webworksbyfaiz@gmailcom>
2024-11-05 12:42:33 +0530

    Added `w-fit` class to `button` element in `ExpandedItemsButton` component.

--- src/components/Buttons/ExpandedItemsButton.jsx
+++ src/components/Buttons/ExpandedItemsButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ExpandedItemsButton = () => {
   return (
-    <button className='flex items-center justify-between bg-white rounded-full border border-[#F2F4F5]'>
+    <button className='flex w-fit items-center justify-between bg-white rounded-full border border-[#F2F4F5]'>
         <p className='bg-white text-[#25245F] font-medium py-2 px-3 rounded-full'>Reject</p>
         <p className='bg-[#0A78CD] text-white font-medium py-2 px-3 rounded-full box-shadow'>Match</p>
     </button>
